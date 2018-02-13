1f73b61819211558071d0708f83de1360997e226
GXTAO <3157188714@qq.com>
2018-02-13 23:49:56 +0800

    shell

--- readme.txt
+++ readme.txt
@@ -13,4 +13,13 @@ shell 基本运算符
         判断两数右边大小      boolean  -gt/-lt
         判断两数左边大小      boolean  -ge/-le
 
+    布尔运算符
+        ！  取非      [ !false ]  true
+        -o  或运算    [ $a -lt 20 -o $b -gt 100 ]  true
+        -a  与运算    [ $a -lt 20 -a $b -gt 100 ]  false
+    逻辑运算符
+        &&  逻辑AND   [[ $a -lt 100 && $b -gt 100 ]]  false
+        ||  逻辑OR    [[ $a -lt 100 || $b -gt 100 ]]  true
+
+
 
--- test.sh
+++ test.sh
@@ -34,3 +34,59 @@ if [ $a != $b ]
 then
     echo "a不等于b"
 fi
+
+#*******判断相等***#
+if [ $a -eq $b ]
+then
+    echo "$a -ne $b"
+else
+    echo "$a -eq $b: a 不等于 b"
+fi
+
+#***********大于右边**********#
+if [ $a -gt $b ]
+then
+    echo "$a -gt $b: a 大于 b"
+else
+    echo "$a -lt $b: a 小于 b"
+fi
+
+#*************取非*************#
+if [ $a != $b ]
+then
+    echo "$a != $b:a 不等于 b"
+else
+    echo "$a != $b:a 等于 b"
+fi
+
+#*************与运算***************#
+if [ $a -lt 100 -a $b -gt 15 ]
+then
+    echo "$a<100||$b>15：true "
+else
+    echo "$a<100||$b>15：false"
+fi
+
+#*************或运算***************#
+if [ $a -lt 100 -o $b -gt 100 ]
+then
+    echo "$a<100||$b>100:true"
+else
+    echo "$a<100||$b>100:false"
+fi
+
+#*************逻辑AND***************#
+if [[ $a -lt 100 && $b -gt 100 ]]
+then
+    echo "true"
+else
+    echo "false"
+fi
+
+#*************逻辑OR****************#
+if [[ $a -lt 100 ||$b -gt 100 ]]
+then
+    echo "true"
+else
+    echo "false"
+fi
